6e91350dc388a59c9d16fcb7ddab35826373429a
DSauthier <danielsauthier@live.com>
2020-04-07 19:11:17 -0400

    asdqwe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Main from './Main';
 import './App.css';
+import SHA256 from "crypto-js/sha256";
+var bcrypt = require("bcryptjs");
+
 
 function App() {
+
+  const saltRounds = 10;
+  const plainPassword1 = "HelloWorld";
+  const plainPassword2 = "helloworld";
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash1 = bcrypt.hashSync(plainPassword1, salt);
+  const hash2 = bcrypt.hashSync(plainPassword2, salt);
+  console.log("Hash 1 -", hash1);
+  console.log("Hash 2 -", hash2);
   return (
     <div className="App">
       <Main/>
+        
     </div>
   );
 }
